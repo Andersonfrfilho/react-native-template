abe8af6396baf684052bdc8d0e3b15119d200787
Anderson <Andersonfrfilho@gmail.com>
2025-08-15 01:51:30 -0300

    fix: correct import babel

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-/**
- * @format
- */
-
+import 'reflect-metadata';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import 'reflect-metadata';
 
 AppRegistry.registerComponent(appName, () => App);
